5e2fa0f6fc90da90f064ae84dbd7922899ce69be
Peter Inglesby <peter.inglesby@gmail.com>
2021-03-02 09:40:09 +0000

    Get column indices from headers

--- analysis/count_occurrences.py
+++ analysis/count_occurrences.py
@@ -4,21 +4,17 @@ import json
 with open("output/input.csv") as f:
     reader = csv.reader(f)
     headers = next(reader)
-    assert headers == [
-        "registered",
-        "has_died",
-        "asthma_dx_today",
-        "asthma_rx_today",
-        "patient_id",
-    ]
+
+    asthma_dx_ix = headers.index("asthma_dx_today")
+    asthma_rx_ix = headers.index("asthma_rx_today")
 
     asthma_dx_today = 0
     asthma_rx_today = 0
 
     for row in reader:
-        if row[2] == "1":
+        if row[asthma_dx_ix] == "1":
             asthma_dx_today += 1
-        if row[3] == "1":
+        if row[asthma_rx_ix] == "1":
             asthma_rx_today += 1
 
 output = {"asthma_dx_today": asthma_dx_today, "asthma_rx_today": asthma_rx_today}
